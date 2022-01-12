b8a5ee6d897788f5b2ff9db257b68c93b989c589
Khurshidbek Kobilov <kabilovhurshid5@gmail.com>
2022-01-12 13:50:36 +0500

    users data created

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,11 +15,37 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::create([
-            'name' => 'Elte-Stadium Admin',
-            'email' => 'admin@eltestadium.hu',
-            'password' => Hash::make('admin'),
-            'is_admin' => true
-        ]);
+        $users = [
+            [
+                'name' => 'Elte-Stadium Admin',
+                'email' => 'admin@eltestadium.hu',
+                'password' => Hash::make('admin'),
+                'is_admin' => true
+            ],
+            [
+                'name' => 'Jack',
+                'email' => 'jack@eltestadium.hu',
+                'password' => Hash::make('user'),
+            ],
+            [
+                'name' => 'Alex',
+                'email' => 'alex@eltestadium.hu',
+                'password' => Hash::make('user'),
+            ],
+            [
+                'name' => 'Barry',
+                'email' => 'barry@eltestadium.hu',
+                'password' => Hash::make('user'),
+            ],
+            [
+                'name' => 'Soliev',
+                'email' => 'soliev@eltestadium.hu',
+                'password' => Hash::make('soliev'),
+            ]
+        ];
+        
+        foreach($users as $user) {
+            User::create($user);
+        }
     }
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Elte Matches</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
