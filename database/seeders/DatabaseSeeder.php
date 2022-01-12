9764548f11dc576fcf1af67a7e7c247835a0a239
Khurshidbek Kobilov <kabilovhurshid5@gmail.com>
2022-01-12 15:22:48 +0500

    games data created

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Comment;
+use App\Models\Game;
 use App\Models\Team;
 use App\Models\User;
 use Illuminate\Database\Seeder;
@@ -46,8 +47,8 @@ class DatabaseSeeder extends Seeder
                 'password' => Hash::make('soliev'),
             ]
         ];
-        
-        foreach($users as $user) {
+
+        foreach ($users as $user) {
             User::create($user);
         }
 
@@ -79,13 +80,13 @@ class DatabaseSeeder extends Seeder
             ]
         ];
 
-        foreach($teams as $team) {
+        foreach ($teams as $team) {
             Team::create($team);
         }
 
         // Creating favorite teams
-        User::whereIn('id', [1,5])->get()->each(function($user) {
-            $user->teams()->sync([2,3,5]);
+        User::whereIn('id', [1, 5])->get()->each(function ($user) {
+            $user->teams()->sync([2, 3, 5]);
         });
 
         // Creating comments
@@ -99,16 +100,304 @@ class DatabaseSeeder extends Seeder
                 'text' => 'A football team is a like a beautiful woman, when you do not tell her, she forgets she is beautiful.',
                 'team_id' => 3,
                 'user_id' => 5
-            ] ,
+            ],
             [
                 'text' => 'A good football team plays offense and defense. You have to be aggressive and disrupt.',
                 'team_id' => 3,
                 'user_id' => 2
-            ]  
+            ]
         ];
 
-        foreach($comments as $comment) {
+        foreach ($comments as $comment) {
             Comment::create($comment);
+        }
+
+        // Creating games
+        $games = [
+            [
+                'host_team_id' => 1,
+                'guest_team_id' => 2,
+                'score' => '1:0'
+            ],
+            [
+                'host_team_id' => 1,
+                'guest_team_id' => 3,
+                'score' => '1:1'
+            ],
+            [
+                'host_team_id' => 1,
+                'guest_team_id' => 4,
+                'score' => '1:2'
+            ],
+            [
+                'host_team_id' => 1,
+                'guest_team_id' => 5,
+                'score' => '0:0'
+            ],
+            [
+                'host_team_id' => 1,
+                'guest_team_id' => 6,
+                'score' => '2:1'
+            ],
+            [
+                'host_team_id' => 1,
+                'guest_team_id' => 7,
+                'score' => '3:2'
+            ],
+            [
+                'host_team_id' => 1,
+                'guest_team_id' => 8,
+                'score' => '3:5'
+            ],
+            [
+                'host_team_id' => 2,
+                'guest_team_id' => 1,
+                'score' => '1:0'
+            ],
+            [
+                'host_team_id' => 2,
+                'guest_team_id' => 3,
+                'score' => '1:1'
+            ],
+            [
+                'host_team_id' => 2,
+                'guest_team_id' => 4,
+                'score' => '1:2'
+            ],
+            [
+                'host_team_id' => 2,
+                'guest_team_id' => 5,
+                'score' => '0:0'
+            ],
+            [
+                'host_team_id' => 2,
+                'guest_team_id' => 6,
+                'score' => '2:1'
+            ],
+            [
+                'host_team_id' => 2,
+                'guest_team_id' => 7,
+                'score' => '3:2'
+            ],
+            [
+                'host_team_id' => 2,
+                'guest_team_id' => 8,
+                'score' => '3:5'
+            ],
+            [
+                'host_team_id' => 3,
+                'guest_team_id' => 1,
+                'score' => '1:0'
+            ],
+            [
+                'host_team_id' => 3,
+                'guest_team_id' => 2,
+                'score' => '1:1'
+            ],
+            [
+                'host_team_id' => 3,
+                'guest_team_id' => 4,
+                'score' => '1:2'
+            ],
+            [
+                'host_team_id' => 3,
+                'guest_team_id' => 5,
+                'score' => '0:0'
+            ],
+            [
+                'host_team_id' => 3,
+                'guest_team_id' => 6,
+                'score' => '2:1'
+            ],
+            [
+                'host_team_id' => 3,
+                'guest_team_id' => 7,
+                'score' => '3:2'
+            ],
+            [
+                'host_team_id' => 3,
+                'guest_team_id' => 8,
+                'score' => '3:5'
+            ],
+            [
+                'host_team_id' => 4,
+                'guest_team_id' => 1,
+                'score' => '1:0'
+            ],
+            [
+                'host_team_id' => 4,
+                'guest_team_id' => 2,
+                'score' => '1:1'
+            ],
+            [
+                'host_team_id' => 4,
+                'guest_team_id' => 3,
+                'score' => '1:2'
+            ],
+            [
+                'host_team_id' => 4,
+                'guest_team_id' => 5,
+                'score' => '0:0'
+            ],
+            [
+                'host_team_id' => 4,
+                'guest_team_id' => 6,
+                'score' => '2:1'
+            ],
+            [
+                'host_team_id' => 4,
+                'guest_team_id' => 7,
+                'score' => '3:2'
+            ],
+            [
+                'host_team_id' => 4,
+                'guest_team_id' => 8,
+                'score' => '3:5'
+            ],
+            [
+                'host_team_id' => 5,
+                'guest_team_id' => 1,
+                'score' => '0:0'
+            ],
+            [
+                'host_team_id' => 5,
+                'guest_team_id' => 2,
+                'score' => '1:0'
+            ],
+            [
+                'host_team_id' => 5,
+                'guest_team_id' => 3,
+                'score' => '1:1'
+            ],
+            [
+                'host_team_id' => 5,
+                'guest_team_id' => 4,
+                'score' => '1:2'
+            ],
+            [
+                'host_team_id' => 5,
+                'guest_team_id' => 6,
+                'score' => '2:1'
+            ],
+            [
+                'host_team_id' => 5,
+                'guest_team_id' => 7,
+                'score' => '3:2'
+            ],
+            [
+                'host_team_id' => 5,
+                'guest_team_id' => 8,
+                'score' => '3:5'
+            ],
+            [
+                'host_team_id' => 6,
+                'guest_team_id' => 1,
+                'score' => '1:0'
+            ],
+            [
+                'host_team_id' => 6,
+                'guest_team_id' => 2,
+                'score' => '1:1'
+            ],
+            [
+                'host_team_id' => 6,
+                'guest_team_id' => 3,
+                'score' => '1:2'
+            ],
+            [
+                'host_team_id' => 6,
+                'guest_team_id' => 4,
+                'score' => '0:0'
+            ],
+            [
+                'host_team_id' => 6,
+                'guest_team_id' => 5,
+                'score' => '2:1'
+            ],
+            [
+                'host_team_id' => 6,
+                'guest_team_id' => 7,
+                'score' => '3:2'
+            ],
+            [
+                'host_team_id' => 6,
+                'guest_team_id' => 8,
+                'score' => '3:5'
+            ],
+            [
+                'host_team_id' => 7,
+                'guest_team_id' => 1,
+                'score' => '3:2'
+            ],
+            [
+                'host_team_id' => 7,
+                'guest_team_id' => 2,
+                'score' => '1:0'
+            ],
+            [
+                'host_team_id' => 7,
+                'guest_team_id' => 3,
+                'score' => '1:1'
+            ],
+            [
+                'host_team_id' => 7,
+                'guest_team_id' => 4,
+                'score' => '1:2'
+            ],
+            [
+                'host_team_id' => 7,
+                'guest_team_id' => 5,
+                'score' => '0:0'
+            ],
+            [
+                'host_team_id' => 7,
+                'guest_team_id' => 6,
+                'score' => '2:1'
+            ],
+            [
+                'host_team_id' => 7,
+                'guest_team_id' => 8,
+                'score' => '3:5'
+            ],
+            [
+                'host_team_id' => 8,
+                'guest_team_id' => 1,
+                'score' => '1:0'
+            ],
+            [
+                'host_team_id' => 8,
+                'guest_team_id' => 2,
+                'score' => '1:1'
+            ],
+            [
+                'host_team_id' => 8,
+                'guest_team_id' => 3,
+                'score' => '1:2'
+            ],
+            [
+                'host_team_id' => 8,
+                'guest_team_id' => 4,
+                'score' => '0:0'
+            ],
+            [
+                'host_team_id' => 8,
+                'guest_team_id' => 5,
+                'score' => '2:1'
+            ],
+            [
+                'host_team_id' => 8,
+                'guest_team_id' => 6,
+                'score' => '3:5'
+            ],
+            [
+                'host_team_id' => 8,
+                'guest_team_id' => 7,
+                'score' => '3:2'
+            ]
+        ];
+
+        foreach ($games as $game) {
+            Game::create($game);
         }
     }
 }
