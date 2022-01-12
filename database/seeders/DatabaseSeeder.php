1a4c6c497b69efb4aedaae3c4ddb99c9d723a1ac
Khurshidbek Kobilov <kabilovhurshid5@gmail.com>
2022-01-12 11:03:19 +0500

    admin created

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +15,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::create([
+            'name' => 'Elte-Stadium Admin',
+            'email' => 'admin@eltestadium.hu',
+            'password' => Hash::make('admin'),
+            'is_admin' => true
+        ]);
     }
 }
